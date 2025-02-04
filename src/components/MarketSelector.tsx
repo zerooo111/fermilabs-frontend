e4bca552eb85336fd76c9f1c11d16a43e5bd599e
NISHIKANT <nishikantraj1977@gmail.com>
2025-02-04 20:43:02 +0530

    Placeholder image added for eaach market coin

--- src/components/MarketSelector.tsx
+++ src/components/MarketSelector.tsx
@@ -11,25 +11,70 @@ import { ChevronDown, Search } from 'lucide-react';
 
 const MarketSelector = () => {
   const inputRef = useRef(null);
-  const [selectedMarket, setSelectedMarket] = useState('JUP/USDC');
+  const [selectedMarket, setSelectedMarket] = useState({
+    coin: 'JUP/USDC',
+    img: 'https://wsrv.nl/?w=128&h=128&default=1&url=https%3A%2F%2Fimage-cdn.solana.fm%2Fimages%2F%3FimageUrl%3Dhttps%3A%2F%2Fstatic.jup.ag%2Fjup%2Ficon.png',
+  });
+
   const [searchQuery, setSearchQuery] = useState('');
+
   const dummyMarket = [
-    'SOL/USDC',
-    'WETH/USDC',
-    'WBTC/USDC',
-    'cbBTC/USDC',
-    'Bonk/USDC',
-    '$WIF/USDC',
-    'POPCAT/USDC',
-    'MOODENG/USDC',
-    'GOAT/USDC',
-    'JUP/USDC',
-    'JTO/USDC',
-    'PYTH/USDC',
-    'DRIFT/USDC',
+    {
+      coin: 'SOL/USDC',
+      img: 'https://wsrv.nl/?w=128&h=128&default=1&url=https%3A%2F%2Fraw.githubusercontent.com%2Fsolana-labs%2Ftoken-list%2Fmain%2Fassets%2Fmainnet%2FSo11111111111111111111111111111111111111112%2Flogo.png',
+    },
+    {
+      coin: 'WETH/USDC',
+      img: 'https://wsrv.nl/?w=128&h=128&default=1&url=https%3A%2F%2Fraw.githubusercontent.com%2Fsolana-labs%2Ftoken-list%2Fmain%2Fassets%2Fmainnet%2F7vfCXTUXx5WJV5JADk17DUJ4ksgau7utNKj4b963voxs%2Flogo.png',
+    },
+    {
+      coin: 'WBTC/USDC',
+      img: 'https://wsrv.nl/?w=128&h=128&default=1&url=https%3A%2F%2Fshdw-drive.genesysgo.net%2F8LmPHjD6fcsBFd27cWM5Zcqbp5FQMjpVSStVZjx4vzFQ%2Fbtcportal.PNG',
+    },
+    {
+      coin: 'cbBTC/USDC',
+      img: 'https://wsrv.nl/?w=128&h=128&default=1&url=https%3A%2F%2Fipfs.io%2Fipfs%2FQmZ7L8yd5j36oXXydUiYFiFsRHbi3EdgC4RuFwvM7dcqge',
+    },
+    {
+      coin: 'Bonk/USDC',
+      img: 'https://wsrv.nl/?w=128&h=128&default=1&url=https%3A%2F%2Farweave.net%2FhQiPZOsRZXGXBJd_82PhVdlM_hACsT_q6wqwf5cSY7I%3Fext%3Dpng',
+    },
+    {
+      coin: '$WIF/USDC',
+      img: 'https://wsrv.nl/?w=128&h=128&default=1&url=https%3A%2F%2Fbafkreibk3covs5ltyqxa272uodhculbr6kea6betidfwy3ajsav2vjzyum.ipfs.nftstorage.link',
+    },
+    {
+      coin: 'POPCAT/USDC',
+      img: 'https://wsrv.nl/?w=128&h=128&default=1&url=https%3A%2F%2Farweave.net%2FA1etRNMKxhlNGTf-gNBtJ75QJJ4NJtbKh_UXQTlLXzI',
+    },
+    {
+      coin: 'MOODENG/USDC',
+      img: 'https://wsrv.nl/?w=128&h=128&default=1&url=https%3A%2F%2Fipfs.io%2Fipfs%2FQmf1g7dJZNDJHRQru7E7ENwDjcvu7swMUB6x9ZqPXr4RV2',
+    },
+    {
+      coin: 'GOAT/USDC',
+      img: 'https://wsrv.nl/?w=128&h=128&default=1&url=https%3A%2F%2Fipfs.io%2Fipfs%2FQmapAq9WtNrtyaDtjZPAHHNYmpSZAQU6HywwvfSWq4dQVV',
+    },
+    {
+      coin: 'JUP/USDC',
+      img: 'https://wsrv.nl/?w=128&h=128&default=1&url=https%3A%2F%2Fimage-cdn.solana.fm%2Fimages%2F%3FimageUrl%3Dhttps%3A%2F%2Fstatic.jup.ag%2Fjup%2Ficon.png',
+    },
+    {
+      coin: 'JTO/USDC',
+      img: 'https://wsrv.nl/?w=128&h=128&default=1&url=https%3A%2F%2Fimage-cdn.solana.fm%2Fimages%2F%3FimageUrl%3Dhttps%3A%2F%2Fmetadata.jito.network%2Ftoken%2Fjto%2Fimage',
+    },
+    {
+      coin: 'PYTH/USDC',
+      img: 'https://wsrv.nl/?w=128&h=128&default=1&url=https%3A%2F%2Fpyth.network%2Ftoken.svg',
+    },
+    {
+      coin: 'DRIFT/USDC',
+      img: 'https://wsrv.nl/?w=128&h=128&default=1&url=http%3A%2F%2Fmetadata.drift.foundation%2Fdrift.png',
+    },
   ];
+
   const filteredMarkets = dummyMarket.filter(market =>
-    market.toLowerCase().includes(searchQuery.toLowerCase())
+    market['coin'].toLowerCase().includes(searchQuery.toLowerCase())
   );
   const handleSearchChange = (e: any) => {
     setSearchQuery(e.target.value);
@@ -40,7 +85,10 @@ const MarketSelector = () => {
         <DropdownMenuTrigger asChild>
           <div className="mr-20">
             <Button variant="outline" className="w-full">
-              {selectedMarket}
+              <span className="w-[25px] h-[25px] rounded-full overflow-hidden">
+                <img src={selectedMarket.img} alt="" className="w-full h-full object-cover" />
+              </span>
+              {selectedMarket.coin}
               <ChevronDown />
             </Button>
           </div>
@@ -71,11 +119,14 @@ const MarketSelector = () => {
                   <DropdownMenuItem
                     key={index}
                     onClick={() => {
-                      setSelectedMarket(market);
+                      setSelectedMarket({ coin: market['coin'], img: market.img });
                       setSearchQuery('');
                     }}
                   >
-                    {market}
+                    <span className="w-[25px] h-[25px] rounded-full overflow-hidden">
+                      <img src={market['img']} alt="" className="w-full h-full object-cover" />
+                    </span>
+                    {market['coin']}
                   </DropdownMenuItem>
                 ))
               ) : (
