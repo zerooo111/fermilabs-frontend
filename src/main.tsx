465cf3f8d18a1293f89910a15f4806032c660f6a
zerooo111 <zero@fermilabs.xyz>
2025-04-11 13:46:56 +0530

    chore: wrap App component in StrictMode for improved development practices

--- src/main.tsx
+++ src/main.tsx
@@ -4,5 +4,10 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './app';
 import './index.css';
+import { StrictMode } from 'react';
 
-createRoot(document.getElementById('root')!).render(<App />);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
