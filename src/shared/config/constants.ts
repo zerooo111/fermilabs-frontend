7b839aa85f645089b7f3d21c0770f1f9a548cb7d
zerooo111 <zero@fermilabs.xyz>
2025-08-19 07:43:55 +0530

    convert to https

--- src/shared/config/constants.ts
+++ src/shared/config/constants.ts
@@ -6,7 +6,7 @@ import { PublicKey } from '@solana/web3.js';
 // config -> network ( devnet / mainnet ) -> programId / rpcUrl , commitment , etc...
 export const config = {
   devnet: {
-    globalSequencerApiUrl: 'http://mtchengine.fermilabs.xyz:8080',
+    globalSequencerApiUrl: 'https://mtchengine.fermilabs.xyz:8080',
     // globalSequencerApiUrl: 'http://54.237.255.40:8080',
     graphApiUrl: 'https://graphapi1.fermilabs.xyz',
     rpcUrl: 'https://api.devnet.solana.com',
