eda5b4360ec63b9c0602288010239bc3f85bbdc0
zerooo111 <zero@fermilabs.xyz>
2026-04-24 18:27:08 +0530

    tooltip: increase default sideOffset to 6 for breathing room

--- src/shared/ui/tooltip.tsx
+++ src/shared/ui/tooltip.tsx
@@ -32,7 +32,7 @@ function TooltipTrigger({ ...props }: React.ComponentProps<typeof TooltipPrimiti
 
 function TooltipContent({
   className,
-  sideOffset = 0,
+  sideOffset = 6,
   children,
   ...props
 }: React.ComponentProps<typeof TooltipPrimitive.Content>) {
